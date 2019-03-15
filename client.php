8214d2a36e8eeba983d61a71c5a1af25b6b33b98
Javier Muñoz <javier@greicodex.com>
2019-03-15 17:02:02 -0400

    Some eyecandy :)

--- client.php
+++ client.php
@@ -1,9 +1,9 @@
 <?php
 $playerid=$_GET['id'];
 $options = [
-    'Piedra',
-    'Papel',
-    'Tijera'
+    'rock',
+    'paper',
+    'scissors'
 ];
 
 $draw=[];
@@ -23,24 +23,38 @@ for($i=0;$i<3;$i++) {
 // pintar botones
 // Con Ajax de accion
 ?>
-<script lang="javascript">
-    var turnos=0;
-    function jugar(el) {
-        var xhr=new XMLHttpRequest();
-        xhr.open('POST','server.php');
-        xhr.send(JSON.stringify({
-            action: 'jugar',
-            player: 'player<?php echo $playerid; ?>',
-            card: el.innerText
-        }));
-        el.disabled=true;
-        turnos++;
-        if(turnos >= 3) {
-            setTimeout(function() {location.reload(); },1000);
-        }
-    }
-</script>
-<h1><?php echo "Jugador $playerid"; ?></h1>
-<button onclick="jugar(this);"><?php echo $cards[0]?></button>
-<button onclick="jugar(this);"><?php echo $cards[1]?></button>
-<button onclick="jugar(this);"><?php echo $cards[2]?></button>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Rock, Paper, Scissors</title>
+        <script lang="javascript">
+            var turnos=0;
+            function jugar(el) {
+                var xhr=new XMLHttpRequest();
+                xhr.open('POST','server.php');
+                xhr.send(JSON.stringify({
+                    action: 'jugar',
+                    player: 'player<?php echo $playerid; ?>',
+                    card: el.innerText
+                }));
+                el.disabled=true;
+                turnos++;
+                if(turnos >= 3) {
+                    setTimeout(function() {location.reload(); },1000);
+                }
+            }
+        </script>
+        <link rel="stylesheet" href="assets/styles.css"/>
+    </head>
+    <body>
+        <div class="container">
+        <h1><?php echo "Jugador $playerid"; ?></h1>
+        <h2>Pick Your Play!</h2>
+        <div class="hand-options">
+            <button class="hand" type="image" onclick="jugar(this);" style="background-image: url('assets/images/<?php echo $cards[0]?>.png')"><?php echo $cards[0]?></button>
+            <button class="hand" type="image" onclick="jugar(this);" style="background-image: url('assets/images/<?php echo $cards[1]?>.png')"><?php echo $cards[1]?></button>
+            <button class="hand" type="image" onclick="jugar(this);" style="background-image: url('assets/images/<?php echo $cards[2]?>.png')"><?php echo $cards[2]?></button>
+        </div>
+        </div>
+    </body>
+</html>
--- server.php
+++ server.php
@@ -8,44 +8,37 @@ function gameLogic() {
     global $puntos;
     if(null!=$estado['player1'] && null!=$estado['player2']) { // Fin de match
         if($estado['player1']==$estado['player2']) 
-            echo "Empate";
-        else if($estado['player1']=='Piedra' &&  $estado['player2']=='Tijera') {
-            echo "Gano 1";
+            $result="Empate";
+        else if($estado['player1']=='Piedra' &&  $estado['player2']=='scissors') {
+            $result= "Gano Player 1";
             $puntos['player1']++;
-        } else if($estado['player1']=='Tijera' &&  $estado['player2']=='Papel') {
-            echo "Gano 1";
+        } else if($estado['player1']=='Tijera' &&  $estado['player2']=='paper') {
+            $result= "Gano Player 1";
             $puntos['player1']++;
-        } else if($estado['player1']=='Papel' &&  $estado['player2']=='Piedra') {
-            echo "Gano 1";
+        } else if($estado['player1']=='Papel' &&  $estado['player2']=='rock') {
+            $result= "Gano Player 1";
             $puntos['player1']++;
         } else {
-            echo "Gano 2";
+            $result= "Gano Player 2";
             $puntos['player2']++;
         }
         //
         $_SESSION['endturn']=true;
         $_SESSION['turnos']++;
         if($_SESSION['turnos'] >=3 ) {
-            echo "<br>FIN DE JUEGO";
             $_SESSION['endgame'] = true;
         }
     }else if(null!=$estado['player1'] || null!=$estado['player2']) { // Espero por otro jugador
-        echo "Esperando por Jugador";
+        $result= "Esperando por Jugador";
     }else { // Vacio
-        echo "Esperando por Jugadores";
+        $result= "Esperando por Jugadores";
     }
+    return array($_SESSION['endturn'],$_SESSION['endgame'],$result,$flash);
 }
 
-function pintarTablero() {
-    global $estado;
-    global $puntos;
-    echo "<h1>Player 1: {$puntos['player1']}, Player 2: {$puntos['player2']} </h1>";
-    echo "<h2>";
-    
-    echo "</h2>";
-    echo '<meta http-equiv="refresh" content="5">';
-    
-}
+
+
+
 function getState() {
     global $estado;
     global $puntos;
@@ -55,53 +48,83 @@ function getState() {
     }
     $estado = unserialize(file_get_contents('cur_match.txt'));
     $puntos = unserialize(file_get_contents('points.txt'));
-
-
 }
+
 function saveState() {
     global $estado;
     global $puntos;
     file_put_contents('cur_match.txt',serialize($estado));
     file_put_contents('points.txt',serialize($puntos));
 }
+
 function resetMatch() {
     global $estado;
     $estado=array('player1'=>null,'player2'=>null);
     
 }
+
 function resetGame() {
     global $puntos;
     global $estado;
-    $estado=array('player1'=>null,'player2'=>null);
-    $puntos=array('player1'=>0,'player2'=>0);
+    $estado=array('player1'=> null,'player2'=> null);
+    $puntos=array('player1'=> 0,'player2'=> 0);
     $_SESSION['turnos']=0;
 }
+
 function recibirJugada($data) {
     global $estado;
-    $estado[$data['player']]=$data['card'];
+    $estado[$data['player']] = $data['card'];
 }
-
 
 if($_SERVER['REQUEST_METHOD']=='POST') {
-    error_log('Recbi post');
+    error_log('Recibi post');
     $data = json_decode(file_get_contents('php://input'),true);
     recibirJugada($data);
-}
-else if($_GET['reset']=='1') {
+} else if($_GET['reset']=='1') {
     resetGame();
     header('Location: /server.php');
-}else {
-    pintarTablero();
-    if($_SESSION['endturn']==true) {
-        $_SESSION['endturn']=false;
-        resetMatch();
-    }
-    if($_SESSION['endgame'] == true) {
-        $_SESSION['endgame']=false;
-        resetGame();
-    }
+} else {
+    pintarTablero(gameLogic());
 }
 
 
 saveState();
-
+function pintarTablero($board) {
+    global $estado;
+    global $puntos;
+    list($endturn,$endgame,$result,$flash) = $board;
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+    <title>Rock, Paper, Scissors</title>
+    <link rel="stylesheet" href="assets/styles.css"/>
+    <meta http-equiv="refresh" content="5">
+    </head>
+    <body>
+    <div class="container">
+    <h1>BATTLE NOW!</h1>
+    <h2>Player 1: <?php echo $puntos['player1'] ?>, Player 2: <?php echo $puntos['player2']?> </h2>
+    <?php if($endturn):?>
+    <div class="hand-options">
+            <button class="hand" type="image" onclick="javascript:void();" style="background-image: url('assets/images/<?php echo $estado['player1']?>.png')"><?php echo $estado['player1']?></button>
+            <button class="hand" type="image" onclick="javascript:void();" style="background-image: url('assets/images/<?php echo $estado['player2']?>.png')"><?php echo $estado['player2']?></button>
+    </div>
+        <h1><?php echo $result ?></h1>
+        <h1><?php echo $flash ?></h1>
+    <?php else: ?>
+        <h1><?php echo $result ?></h1>
+    <?php endif;?>
+    </div>
+    </body>
+</html>
+<?php 
+    if( $_SESSION['endturn']==true ) {
+        $_SESSION['endturn']=false;
+        resetMatch();
+    }
+    if( $_SESSION['endgame'] == true ) {
+        $_SESSION['endgame']=false;
+        resetGame();
+    }
+}
